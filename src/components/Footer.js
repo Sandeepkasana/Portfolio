8596eda1a5a4cb0407b4fbe5a09118ac079c2a2f
Sandeep kasana <sdkasana26@gmail.com>
2025-04-23 11:28:01 +0530

    code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by Sandeep Singh</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} VB</h3>
+          <h3>Copyright © {year} SS</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
