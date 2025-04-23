7d41477eb29f8159f00806a3d4b3d3a020ffee5d
Sandeep kasana <sdkasana26@gmail.com>
2025-04-23 10:51:41 +0530

    Code

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="GoTogether"
               description="Engineered a feature-rich ride-sharing platform using React.js, Vite, and TailwindCSS, integrating real-time booking, interactive maps with proximity-based matching, user ratings, and notifications to streamline transportation, optimize route selection, and enhance user engagement and experience."
-              ghLink="https://github.com/vipulbeniwal01/GoTogether"
+              ghLink="https://github.com/sandeepkasana/GoTogether"
               demoLink="https://gotogether-carpool.vercel.app"
             />
           </Col>
@@ -38,8 +38,8 @@ function Projects() {
               isBlog={false}
               title="Burger House Website"
               description="The Burger House Website is an interactive and visually appealing site for a burger restaurant, built using HTML, CSS, and JavaScript. It features smooth scroll animations with ScrollReveal, enhancing user experience and showcasing the restaurant’s menu and services effectively."
-              ghLink="https://github.com/vipulbeniwal01/BURGER-HOUSE-WEBSITE"
-              demoLink="https://vipulbeniwal01.github.io/BURGER-HOUSE-WEBSITE/"
+              ghLink="https://github.com/sandeepkasana/BURGER-HOUSE-WEBSITE"
+              demoLink="https://sandeepkasana.github.io/BURGER-HOUSE-WEBSITE/"
             />
           </Col>
 
@@ -49,8 +49,8 @@ function Projects() {
               isBlog={false}
               title="N Queen Visualiser"
               description="The N-Queen Visualizer is a project that visually demonstrates the solution to the N-Queen problem, where N queens are placed on an N×N chessboard without threatening each other. Built using HTML, CSS, and JavaScript, it provides a dynamic, step-by-step visualization of the algorithm solving the puzzle."
-              ghLink="https://github.com/vipulbeniwal01/N-Queen-Visualiser"
-              demoLink="https://vipulbeniwal01.github.io/N-Queen-Visualiser/"
+              ghLink="https://github.com/sandeepkasana/N-Queen-Visualiser"
+              demoLink="https://sandeepkasana.github.io/N-Queen-Visualiser/"
             />
           </Col>
 
@@ -60,8 +60,8 @@ function Projects() {
               isBlog={false}
               title="Sorting Visulizer"
               description="Sorting Algorithm Visualizer A web application built using HTML, CSS, and JavaScript to visualize classic sorting algorithms such as Bubble Sort, Insertion Sort, Selection Sort, Merge Sort, and Quick Sort. This live-sorting visualizer helps in understanding how different sorting algorithms work."
-              ghLink="https://github.com/vipulbeniwal01/Sorting-Visualizer"
-              demoLink="https://vipulbeniwal01.github.io/Sorting-Visualizer/"              
+              ghLink="https://github.com/sandeepkasana/Sorting-Visualizer"
+              demoLink="https://sandeepkasana.github.io/Sorting-Visualizer/"              
             />
           </Col>
 
